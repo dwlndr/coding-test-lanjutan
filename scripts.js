e916264c68a0c84c6b6687a108ab31d6bb61e76e
Ardhilla Eka <ardhillaeka83@gmail.com>
2024-09-09 19:33:55 +0700

    15  menit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
 function hitungkarakter(text) {
-    let 
+    let jumlahkarakter = {}
+    
+    for (let i = 0; i < text.length; i++) {
+        let karakter = text[i];
+
+        if (jumlahkarakter[karakter]) {
+            
+        }
+    }
 }
 
+text = "Hello"
+console.log(text.length)
 // 
